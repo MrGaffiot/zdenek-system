6addc5ace31d5f7635625703db90c44ef454bc93
MrWalper <MrWalper@proton.me>
2025-03-27 21:41:16 +0100

    Updated inputting system

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
 import databaseHandler
 import jsonHandler
 
@@ -10,3 +9,11 @@ images = jsonHandler.read()["posts"]
 @app.route("/")
 def hello_world():
     return render_template('test.html', images=images)
+
+@app.route('/form', methods=['GET', 'POST'])
+def handle_form():
+    if request.method == 'POST':
+        jsonHandler.write([[request.form['pathToImage'], request.form['url'], request.form['description']]])
+        return render_template('test.html', images=images)
+    
+    return render_template('form.html')
