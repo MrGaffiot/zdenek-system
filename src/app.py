da24669ba11eeff0042918f5a20040cf8870bb5b
MrWalper <MrWalper@proton.me>
2025-03-27 19:28:30 +0100

    Added images

--- src/app.py
+++ src/app.py
@@ -4,6 +4,19 @@ import databaseHandler
 
 app = Flask(__name__)
 
+images = [
+    ["1.webp", "https://www.google.com/", "1"],
+    ["1.webp", "https://www.google.com/", "1"],
+    ["1.webp", "https://www.google.com/", "1"],
+    ["1.webp", "https://www.google.com/", "1"],
+    ["1.webp", "https://www.google.com/", "1"],
+    ["2.webp", "https://www.google.com/", "2"],
+    ["3.gif", "https://www.google.com/", "3"],
+    ["4.gif", "https://www.google.com/", "4"],
+    ["5.gif", "https://www.google.com/", "5"],
+    ["6.png", "https://www.google.com/", "6"]
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('test.html')
+    return render_template('test.html', images=images)
